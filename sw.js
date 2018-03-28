99b9db017c11159b89b975ca58a299e74ab537c2
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2018-03-28 08:11:51 +0200

    feat: hijacking request with sw

--- sw.js
+++ sw.js
@@ -1,12 +1,15 @@
-console.log('hello from sw.js');
-
-self.addEventListener('install', (event)=>{
-    console.log(event);
-    console.log("installl!!!!");
-
+self.addEventListener('fetch', (event)=>{
+    event.respondWith(
+        fetch(event.request).then((response)=>{
+            if(response.status == 404) {
+                return new Response("Not found!");
+            }
+            console.log('We have got repsonse: ');
+            console.log(response);
+            return response;
+        }).catch((err)=>{
+            console.log(err);
+            return new Response("Request failed!");
+        })
+    );
 });
-
-// self.addEventListener('fetch',(event) => {
-//     debugger;
-//     //console.log(event.request);
-// });
