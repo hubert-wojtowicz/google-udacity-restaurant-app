f89ba6a741427171659a8db0780f6da62579dbb9
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2018-03-30 07:45:06 +0200

    feat: fonts added to static cache

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 var staticCacheName = 'mws-restaurant-v3'
 
+
 self.addEventListener('install',(event)=>{
     var urlsToCache = [
         '/',
@@ -9,9 +10,13 @@ self.addEventListener('install',(event)=>{
         'js/dbhelper.js',
         'js/restaurant_info.js',
         'css/styles.css',
-        'data/restaurants.json'
+        'data/restaurants.json',
+        'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
+        'https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxK.woff2',
+        'https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu7GxKOzY.woff2',
+        'https://fonts.gstatic.com/s/roboto/v18/KFOlCnqEu92Fr1MmEU9fBBc4.woff2'
     ];
-
+    
     event.waitUntil(
         caches.open(staticCacheName).then((cache)=>{
             return cache.addAll(urlsToCache);
@@ -24,11 +29,9 @@ self.addEventListener('activate',(event)=>{
         caches.keys().then((cacheNames)=>{
             return Promise.all(
                 cacheNames.filter((cacheName)=>{
-                    debugger;
                     return cacheName.startsWith('mws-restaurant-v') && 
                         cacheName != staticCacheName;
                 }).map((cacheName)=>{
-                    debugger;
                     return caches.delete(cacheName);
                 })
             );
@@ -39,9 +42,8 @@ self.addEventListener('activate',(event)=>{
 self.addEventListener('fetch', (event)=>{
     event.respondWith(
         caches.match(event.request).then((response)=>{
-            console.log(event.request.url);
             if(response) {
-                //console.log('Take from cache' + event.request.url);
+                console.log('Take from cache' + event.request.url);
                 return response;
             } 
 
@@ -49,8 +51,7 @@ self.addEventListener('fetch', (event)=>{
                     if(response.status == 404) {
                         return new Response("Not found!");
                     }
-                    // console.log('We have got repsonse: ');
-                    // console.log(response);
+                    console.log('Take from outside world: '+event.request.url);
                     return response;
                 }).catch((err)=>{
                     return new Response(`Request failed! Error msg: ${err}`);
