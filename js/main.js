5ad459c6024ac65d025101a35db2d96e986cdd1a
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2018-04-03 07:16:27 +0200

    fix: headers level

--- js/main.js
+++ js/main.js
@@ -146,7 +146,7 @@ createRestaurantHTML = (restaurant) => {
   image.setAttribute('srcset',srcset);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
