5be687e8fcfd03669bbc8091c36777637b1f3939
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2018-04-03 20:09:31 +0200

    fix: unnecessary sw.js code commented out

--- sw.js
+++ sw.js
@@ -1,31 +1,31 @@
 var cacheName = 'mws-restaurant-v1';
-var urlsToCache = [
-    '/',
-    'sw.js',
-    'index.js',
-    'restaurant.html',
-    'js/dbhelper.js',
-    'js/main.js',
-    'js/dbhelper.js',
-    'js/restaurant_info.js',
-    'css/styles.css',
-    'data/restaurants.json',
-];
+// var urlsToCache = [
+//     '/',
+//     'sw.js',
+//     'index.js',
+//     'restaurant.html',
+//     'js/dbhelper.js',
+//     'js/main.js',
+//     'js/dbhelper.js',
+//     'js/restaurant_info.js',
+//     'css/styles.css',
+//     'data/restaurants.json',
+// ];
 
-self.addEventListener('install',(event)=>{
-    event.waitUntil(
-        caches.open(cacheName).then((cache)=>{
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
+// self.addEventListener('install',(event)=>{
+//     event.waitUntil(
+//         caches.open(cacheName).then((cache)=>{
+//             return cache.addAll(urlsToCache);
+//         })
+//     );
+// });
 
 self.addEventListener('activate',(event)=>{
-    event.waitUntil( 
+    event.waitUntil(
         caches.keys().then((cacheNames)=>{
             return Promise.all(
                 cacheNames.filter((cacheName)=>{
-                    return cacheName.startsWith('mws-restaurant-v') && 
+                    return cacheName.startsWith('mws-restaurant-v') &&
                         cacheName != cacheName;
                 }).map((cacheName)=>{
                     return caches.delete(cacheName);
