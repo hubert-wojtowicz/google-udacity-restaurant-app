bde376ec4acf99609b68e0976e3468abfab2302a
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2018-06-28 20:48:56 +0200

    feat: rating manager added to comment form

--- src/js/commentFormManager.js
+++ src/js/commentFormManager.js
@@ -1,42 +1,56 @@
+import RateManager from './rateManager'; 
 export default class CommentFormManager {
     constructor(restaurant, containerElement, db) {
         this.db = db;
         this.restaurant = restaurant;
         this.containerElement = containerElement;
-        this.formDef = {
+        this.rateManager = null;
+        this.formElem = {
             headerText: null,
+            rateHidden: null,
+            rateManagerContainer: null,
             authorInputText: null,
             commentTextarea: null,
-            rateHidden: null,
             submitBtn: null
         };
-
-        this.emptyStar = '☆';
-        this.fullStar = '★';
 
         this.render();
     }
     render() {
-        this.formDef.headerText = document.createElement('h3');
-        this.formDef.headerText.innerHTML = "Add new review";
+        this.formElem.headerText = document.createElement('h3');
+        this.formElem.headerText.innerHTML = "Add new review";
 
-        this.formDef.authorInputText = document.createElement('input'); 
-        this.formDef.authorInputText.classList.add('review-form-input-text');
+        this.formElem.rateHidden = document.createElement('input');
+        this.formElem.rateHidden.id = 'review-form-rete-hidden';
+        this.formElem.rateHidden.setAttribute('type','hidden');
+        this.formElem.rateHidden.value = 0;
 
-        this.formDef.commentTextarea = document.createElement('textarea');
-        this.formDef.commentTextarea.classList.add('review-form-textarea');
+        this.formElem.rateManagerContainer = document.createElement('div');
+        this.formElem.rateManagerContainer.classList.add('review-form-rate-manager');
+        this.containerElement.appendChild(this.formElem.rateManagerContainer);
+        this.rateManager = new RateManager(this.formElem.rateManagerContainer, this.formElem.rateHidden);  
+                
+        this.formElem.authorInputText = document.createElement('input'); 
+        this.formElem.authorInputText.id = 'review-form-author-input-text';
+        this.formElem.authorInputText.classList.add('review-form-input-text');
+        this.formElem.authorInputText.placeholder = 'Put your name here ...';
+        this.formElem.authorInputText.setAttribute('maxlength', 30);
         
-        this.formDef.rateHidden = document.createElement('input');
-        this.formDef.rateHidden.setAttribute('type','hidden');
+        this.formElem.commentTextarea = document.createElement('textarea');
+        this.formElem.commentTextarea.id = 'review-form-comment-textarea';
+        this.formElem.commentTextarea.classList.add('review-form-textarea');
+        this.formElem.commentTextarea.placeholder = 'Put your comment here ...';
+        this.formElem.commentTextarea.setAttribute('maxlength', 500);
 
-        this.formDef.submitBtn = document.createElement('button');
-        this.formDef.submitBtn.classList.add('review-form-submit');
-        this.formDef.submitBtn.addEventListener('click', this.onSubmit.bind(this));
+        this.formElem.submitBtn = document.createElement('button');
+        this.formElem.submitBtn.id = 'review-form-submit-btn';
+        this.formElem.submitBtn.classList.add('review-form-submit');
+        this.formElem.submitBtn.addEventListener('click', this.onSubmit.bind(this));
         
         // create each form element
-        for(let formElemKey in this.formDef)
+        for(let formElemKey in this.formElem)
         {
-            this.containerElement.appendChild(this.formDef[formElemKey]);  
+            this.containerElement.appendChild(this.formElem[formElemKey]);  
         }
     }
 
--- src/js/rateManager.js
+++ src/js/rateManager.js
@@ -1,4 +1,46 @@
 export default class RateManager {
+    get MAX_RATE() {
+        return 5;
+    }
 
+    constructor(rateManagerContainer, valueElemStorage) {
+        this.rateManagerContainer = rateManagerContainer;
+        this.valueElemStorage = valueElemStorage;
+        this.currentRate = +1;
+        this.emptyStar = '☆';
+        this.fullStar = '★';
 
+        this.initDomStructure();
+    }
+
+    initDomStructure() {
+        for(let i = 0;  i < this.MAX_RATE; i++) {
+            let starElem = document.createElement('button');
+            starElem.innerHTML = (this.currentRate > i) ? this.fullStar : this.emptyStar;
+            starElem.classList.add(i+1);
+            starElem.addEventListener('mouseover', this.onMouseOver.bind(this));
+            starElem.addEventListener('mouseleave', this.onMouseLeave.bind(this));
+            starElem.addEventListener('click', this.onClick.bind(this));
+            this.rateManagerContainer.appendChild(starElem);
+        }
+    }
+
+    onMouseLeave() {
+        for(let i = 0;  i < this.MAX_RATE; i++) {
+            this.rateManagerContainer.children[i].innerHTML = (this.currentRate > i) ? this.fullStar : this.emptyStar;
+        }
+    }
+
+    onMouseOver(event) {
+        let mouseOverIndex = event.target.classList[0];
+        for(let i = 0;  i < this.MAX_RATE; i++) {
+            this.rateManagerContainer.children[i].innerHTML = (mouseOverIndex > i) ? this.fullStar : this.emptyStar;
+        }
+    }
+
+    onClick(event) {
+        this.currentRate = event.target.classList[0];
+        this.valueElemStorage.value = this.currentRate;
+        console.log(`Current rate changed to ${this.valueElemStorage.value}`);
+    }
 }
